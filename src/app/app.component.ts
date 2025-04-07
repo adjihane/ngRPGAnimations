e9422a329468f353144fcf148574314b263df9ca
jihane <2282988@cegepmontpetit.ca>
2025-04-07 08:48:47 -0400

    fct : avant faire attaquer slimey

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,60 @@
 import { Component } from '@angular/core';
-import {transition, trigger, useAnimation} from "@angular/animations";
+import { transition, trigger, useAnimation } from "@angular/animations";
+import { shakeX } from 'ng-animate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger("death", [
+      transition(
+        ":increment",
+        useAnimation(shakeX, { params: { timing: 0.5 } })
+      ),
+    ]),
+  ]
 })
 export class AppComponent {
   slimeIsPresent = false;
+  ng_death = 0;
 
   constructor() {
+  }
+
+  hideSlime() {
+    var element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeIn");
+    element?.classList.add("fadeOut");
   }
 
   spawn() {
     this.slimeIsPresent = true;
     // TODO Animation angular avec forwards
+
+    var element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeOut");
+    element?.classList.add("fadeIn");
+
+    console.log(this)
   }
 
-  death(){
+  death() {
     this.slimeIsPresent = false;
     // TODO Animation angular avec forwards
-
+    this.hideSlime()
+    this.ng_death++
+    console.log(this.ng_death)
     // TODO 2e animation angular en même temps
+
   }
 
-  attack(){
+  attack() {
     // TODO Jouer une animation et augmenter l'intensité du mouvement avec scale
     // TODO Jouer une autre animation avant
   }
 
-  hit(){
+  hit() {
     // TODO Utilisé Animista pour faire une animation différente avec css (wobble)
   }
 }
