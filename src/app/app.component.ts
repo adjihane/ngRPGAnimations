ee0c62771d32c7c49e64b227179459e03516b75f
jihane <2282988@cegepmontpetit.ca>
2025-04-09 15:54:17 -0400

    animation 2 fini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { transition, trigger, useAnimation } from "@angular/animations";
-import { headShake, jello, pulse, rubberBand, shakeX, swing, wobble } from 'ng-animate';
+import { bounce, flip, headShake, jello, pulse, rubberBand, shakeX, swing, wobble } from 'ng-animate';
+import { lastValueFrom, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,9 @@ import { headShake, jello, pulse, rubberBand, shakeX, swing, wobble } from 'ng-a
       [useAnimation(jello, { params: { timing: 0.3, scale: 4.5 } }),
       useAnimation(pulse, { params: { timing: 0.3, scale: 4.5 } }),
       ]),]),
+    trigger("bounce", [transition(":increment", useAnimation(bounce, { params: { timing: 1 } })),]),
+    trigger("shake", [transition(":increment", useAnimation(shakeX, { params: { timing: 0.75 } })),]),
+    trigger("flip", [transition(":increment", useAnimation(flip, { params: { timing: 0.75 } })),]),
   ]
 })
 export class AppComponent {
@@ -19,7 +23,24 @@ export class AppComponent {
   ng_death = 0;
   ng_attack = 0;
   css_hit = false;
+  ng_bounce = 0;
+  ng_shake = 0;
+  ng_flip = 0;
+  css_rotatecenter = false;
+  css_rotatehortop = false;
   constructor() {
+  }
+
+  async BounceShakeFlip() {
+    console.log(this.ng_bounce)
+    this.ng_bounce++;
+
+    await lastValueFrom(timer(1000))
+    this.ng_shake++;
+
+    await lastValueFrom(timer(1000))
+    this.ng_flip++;
+
   }
 
   hideSlime() {
@@ -62,4 +83,26 @@ export class AppComponent {
     setTimeout(() => this.css_hit = false, 0.5 * 1000);
     console.log(this)
   }
+
+
+  Spin() {
+    this.Rotate1();
+  }
+
+  Rotate1() {
+    this.css_rotatecenter = true;
+    setTimeout(() => {
+      this.css_rotatecenter = false;
+      this.Rotate2();
+    }, 1000);
+  }
+
+  Rotate2() {
+    this.css_rotatehortop = true;
+    setTimeout(() => {
+      this.css_rotatehortop = false;
+      this.Rotate1();
+    }, 1000);
+  }
+
 }
